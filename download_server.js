cd9b2de80a9694d65be1183f92850b0990d132b5
CSMAC <moonyojun@gmail.com>
2024-08-05 17:04:29 +0900

    update

--- download_server.js
+++ download_server.js
@@ -9,30 +9,42 @@ const PORT = 90;
 const folderPath = "D:/BIGMACLAB/CRAWLER/BIGMACLAB_MANAGER";
 
 app.get('/files', (req, res) => {
-    fs.readdir(folderPath, (err, files) => {
-      if (err) {
-        return res.status(500).send('Unable to scan directory.');
-      }
-      // .exe 파일만 필터링
-      const exeFiles = files.filter(file => path.extname(file).toLowerCase() === '.exe');
-  
-      // 버전 비교 함수
-      const compareVersions = (a, b) => {
-        const versionA = a.match(/(\d+\.\d+\.\d+)/)[0].split('.').map(Number);
-        const versionB = b.match(/(\d+\.\d+\.\d+)/)[0].split('.').map(Number);
-  
-        for (let i = 0; i < versionA.length; i++) {
-          if (versionA[i] > versionB[i]) return -1;
-          if (versionA[i] < versionB[i]) return 1;
-        }
-        return 0;
+  fs.readdir(folderPath, (err, files) => {
+    if (err) {
+      return res.status(500).send('Unable to scan directory.');
+    }
+
+    // .exe 파일만 필터링
+    const exeFiles = files.filter(file => path.extname(file).toLowerCase() === '.exe');
+
+    // 파일 정보 배열
+    const fileInfoArray = exeFiles.map(file => {
+      const filePath = path.join(folderPath, file);
+      const stats = fs.statSync(filePath); // 파일의 상태 정보 가져오기
+      return {
+        name: file,
+        size: (stats.size / (1024 * 1024)).toFixed(2) + ' MB' // 파일 크기를 MB 단위로 변환
       };
-  
-      // 버전 순으로 정렬
-      exeFiles.sort(compareVersions);
-      res.json(exeFiles);
     });
+
+    // 버전 비교 함수
+    const compareVersions = (a, b) => {
+      const versionA = a.name.match(/(\d+\.\d+\.\d+)/)[0].split('.').map(Number);
+      const versionB = b.name.match(/(\d+\.\d+\.\d+)/)[0].split('.').map(Number);
+
+      for (let i = 0; i < versionA.length; i++) {
+        if (versionA[i] > versionB[i]) return -1;
+        if (versionA[i] < versionB[i]) return 1;
+      }
+      return 0;
+    };
+
+    // 버전 순으로 정렬
+    fileInfoArray.sort(compareVersions);
+    res.json(fileInfoArray);
   });
+});
+
 
 // 파일 다운로드 엔드포인트
 app.get('/download/:filename', (req, res) => {
